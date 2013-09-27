5e433a40aca08a4bfe2e31d4a8bfc53791568c12
wuxin <82828068@qq.com>
2013-09-27 16:17:10 +0800

    访问影射文件的框架

--- source/subsystem/fs/map.c
+++ source/subsystem/fs/map.c
@@ -13,28 +13,18 @@
 #include "cache.h"
 
 /**
-	@brief Create map instance
- */
-struct dmr *fss_map_create()
+	@brief 准备好DBD/DMR等关键数据结构
+
+	@return
+		DB 地址
+	@note
+		本函数保证DB中有有效数据
+*/
+void *fss_map_prepare_dbd(struct fss_file *file, void *process, uoffset file_pos)
 {
 	
 }
 
-/**
-	@brief Map part of file to desired space
- */
-int fss_map_file(struct dmr *map, uoffset file_pos)
-{
-	
-}
-
-/**
-	@brief Unmap the part/total file from desired space
- */
-int fss_map_delete(struct dmr *map, uoffset file_pos)
-{
-	
-}
 
 int fss_map_init()
 {
