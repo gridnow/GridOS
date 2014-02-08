321d5613c25250b2b490bb916029079e6eaf3a1a
wuxin <82828068@qq.com>
2014-02-08 23:00:24 +0800

    Add the network subsystem framework.

---
+++ source/subsystem/network/main.c
@@ -0,0 +1,31 @@
+/**
+	Network framework
+
+	Sihai
+*/
+#include <types.h>
+#include <compiler.h>
+#include <ddk/net.h>
+
+struct nss_hwmgr *hwmgr;
+
+/************************************************************************/
+/* Controller                                                           */
+/************************************************************************/
+static void probe_avaliber_netif()
+{
+	
+}
+
+/* Called now by DSS */
+DLLEXPORT void nss_hwmgr_register(struct nss_hwmgr *mgr)
+{
+	hwmgr = mgr;
+
+	hwmgr->nops->open("eth0");
+}
+
+void nss_main()
+{
+	hwmgr = NULL;
+}
